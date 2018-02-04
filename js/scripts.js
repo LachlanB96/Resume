61fe8b47a96518a830612be69fdfcea4ecd27396
Lachlan Brown <lachlan@lachlanb.com>
2018-02-04 23:35:03 +1100

    Always adding more....

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,25 @@ $(document).ready(function() {
         console.log($(window).scrollTop());
         console.log(window.innerHeight);
 
-        if ($(window).scrollTop() < window.innerHeight) {
-            $('nav').addClass("sticky-top");
+        if ($(window).scrollTop() > window.innerHeight) {
+            $('nav').addClass("navbar-fixed-top");
         }
 
-        if ($(window).scrollTop() > window.innerHeight) {
-            $('nav').removeClass("sticky-top");
+        if ($(window).scrollTop() < window.innerHeight) {
+            $('nav').removeClass("navbar-fixed-top");
+        }
+        beforeContent = 100;
+        if ($(window).scrollTop() > beforeContent) {
+            $('.section-title').addClass("onscreen");
+        }
+    });
+});
+
+$(function () {
+    $('.min-chart#chart-sales').easyPieChart({
+        barColor: "#4caf50",
+        onStep: function (from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
         }
     });
 });
